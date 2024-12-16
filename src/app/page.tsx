50c262de9bc5eda7d41a26fd2f4af56841989221
Remigiusz <remek778790@gmail.com>
2024-12-16 18:40:03 +0100

    quickfix#1

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,10 +1,13 @@
 import OffersList from "@/components/offers/OffersList";
+import { Suspense } from "react";
 
 export default function Home() {
   return (
     <div className="h-full my-6">
       <section className="max-w-screen-2xl mx-auto px-4">
-        <OffersList />
+        <Suspense fallback={<div>Loading...</div>}>
+          <OffersList />
+        </Suspense>
       </section>
     </div>
   );
