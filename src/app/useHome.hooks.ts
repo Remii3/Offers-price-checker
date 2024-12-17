68f7c8dc8fc69c897831323c69ccc55db61d80c5
Remigiusz <remek778790@gmail.com>
2024-12-17 20:03:57 +0100

    test

--- src/app/useHome.hooks.ts
+++ src/app/useHome.hooks.ts
@@ -63,6 +63,7 @@ export function useHome() {
     refetchOnMount: true,
     refetchOnWindowFocus: false,
     refetchOnReconnect: false,
+    staleTime: 0,
   });
 
   const { mutate: deleteAll, isPending: deleteAllIsLoading } = useMutation({
